ed0de88424eee75051c5a35eacc6fe42930ec3f1
Gorecki Piotr <piotr.gorecki@meelogic.com>
2021-03-05 17:18:26 +0100

    initial templates and schelton

--- TwojKomputerowiec/twojKomputerowiec.py
+++ TwojKomputerowiec/twojKomputerowiec.py
@@ -1,16 +1,45 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+app = Flask(__name__, template_folder='Szablony')
+
+posty = [
+    {
+        'autor': 'Piotr Górecki',
+        'tytul': 'Strona w przygotowaniu',
+        'tresc': 'Trwa przygotowanie strony firmowej',
+        'data': '05.03.2021'
+    },
+    {
+        'autor': 'Piotr Górecki',
+        'tytul': 'Firma w przygotowaniu',
+        'tresc': 'Trwa przygotowanie koncecji firmy',
+        'data': '05.03.2021'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 @app.route('/dom')
-def strona_startowa():
-    return '<h1>Strona startowa</h1>'
+def stronaStartowa():
+    return render_template('domowa.html')
 
-@app.route('/o_mnie')
+
+@app.route('/news')
+@app.route('/aktualnosci')
+def aktualnosci():
+    return render_template('aktualnosci.html', posts=posty)
+
+
+@app.route('/contact')
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt.html')
+
+
 @app.route('/about')
-def o_mnie():
-    return '<h1>O mnie</h1>'
+@app.route('/oMnie')
+def oMnie():
+    return render_template('oMnie.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
