6db1f2649ddc1dc8374bb5fa2c74e2be07d6bee7
Gorecki Piotr <piotr.gorecki@meelogic.com>
2021-04-07 17:03:55 +0200

    rest exaple client added

---
+++ TwojKomputerowiec/restClient.py
@@ -0,0 +1,19 @@
+import requests
+serverURL = 'http://piotrek24061988.pythonanywhere.com'
+serverMethod1 = '/autor'
+serverMethod2 = '/email'
+jsonPostMethod2 = {'kontakt':'mieciu@gmail.com',
+                   'temat':'przykladowy temat',
+                   'tresc':'przykladowa tresc'}
+
+resp = requests.get(serverURL + serverMethod1)
+print(resp.status_code)
+print(resp.text)
+
+resp = requests.get(serverURL + serverMethod2)
+print(resp.status_code)
+print(resp.text)
+
+resp = requests.post(serverURL + serverMethod2, json=jsonPostMethod2)
+print(resp.status_code)
+print(resp.text)
