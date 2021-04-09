e9f22bed25961571737c8388afa159ec8ecb4db4
Gorecki Piotr <piotr.gorecki@meelogic.com>
2021-04-09 11:11:12 +0200

    small restclient cleanup

--- TwojKomputerowiec/restClient.py
+++ TwojKomputerowiec/restClient.py
@@ -1,23 +1,17 @@
 import requests
+
 serverURL = 'http://piotrek24061988.pythonanywhere.com'
-serverMethod1 = '/autor'
-serverMethod2 = '/email'
-jsonPostMethod2 = {'kontakt':'mieciu@gmail.com',
-                   'temat':'przykladowy temat',
-                   'tresc':'przykladowa tresc'}
+serverMethods = {1:'/autor', 2:'/email'}
+jsonPostMethod2 = {'kontakt':'mieciu@gmail.com', 'temat':'przykladowy temat', 'tresc':'przykladowa tresc'}
 
-resp = requests.get(serverURL + serverMethod1)
-print(resp.status_code)
-print(resp.text)
+resp = requests.get(serverURL + serverMethods[1])
+print([resp.status_code, resp.text])
 
-resp = requests.get(serverURL + serverMethod2)
-print(resp.status_code)
-print(resp.text)
+resp = requests.get(serverURL + serverMethods[2])
+print([resp.status_code, resp.text])
 
-resp = requests.post(serverURL + serverMethod2, json={})
-print(resp.status_code)
-print(resp.text)
+resp = requests.post(serverURL + serverMethods[2], json={})
+print([resp.status_code, resp.text])
 
-resp = requests.post(serverURL + serverMethod2, json=jsonPostMethod2)
-print(resp.status_code)
-print(resp.text)
+resp = requests.post(serverURL + serverMethods[2], json=jsonPostMethod2)
+print([resp.status_code, resp.text])
