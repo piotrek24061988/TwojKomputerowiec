5e273b778c75655c8877e115cf9e4fa12ae037d4
Gorecki Piotr <piotr.gorecki@meelogic.com>
2021-04-26 18:19:25 +0200

    zegar dodany

---
+++ TwojKomputerowiec/TwojKomputerowiec/static/zegar.js
@@ -0,0 +1,13 @@
+function currentTimeAndDate()
+{
+    var today = new Date();
+    var hours = today.getHours();
+    if(hours < 10) hours = "0" + hours;
+    var minutes = today.getMinutes();
+    if(minutes < 10) minutes = "0" + minutes;
+    var seconds = today.getSeconds();
+    if(seconds < 10) seconds = "0" + seconds;
+    document.getElementById("zegar").innerHTML = hours + ":" + minutes + ":" + seconds;
+
+    setTimeout("currentTimeAndDate()", 500);
+}
